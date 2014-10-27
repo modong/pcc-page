763cb782cb4060a462bb2ad8f85e1672378ee0e0
Mo Dong <modong@Mos-MacBook-Pro.local>
2014-10-26 22:59:43 -0400

    big change

---
+++ js/backtotop.js
@@ -0,0 +1,8 @@
+// Only enable if the document has a long scroll bar
+// Note the window height + offset
+if ( ($(window).height() + 700) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+        // how far to scroll down before link "slides" into view
+        offset: {top:100}
+    });
+}
--- js/clean-blog.js
+++ js/clean-blog.js
@@ -85,7 +85,7 @@ $('#name').focus(function() {
  // * License: MIT <http://opensource.org/licenses/mit-license.php> - see LICENSE file
  // *
  // * http://ReactiveRaven.github.com/jqBootstrapValidation/
- 
+
 
 (function( $ ){
 
@@ -557,7 +557,7 @@ $('#name').focus(function() {
                 // How many errors did we find?
                 if (settings.options.semanticallyStrict && errorsFound.length === 1) {
                   // Only one? Being strict? Just output it.
-                  $helpBlock.html(errorsFound[0] + 
+                  $helpBlock.html(errorsFound[0] +
                     ( settings.options.prependExistingHelpBlock ? $helpBlock.data("original-contents") : "" ));
                 } else {
                   // Multiple? Being sloppy? Glue them together into an UL.
@@ -1030,3 +1030,14 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+// Only enable if the document has a long scroll bar
+// // Note the window height + offset
+ if ( ($(window).height() + 100) < $(document).height() ) {
+     $('#top-link-block').removeClass('hidden').affix({
+             // how far to scroll down before link "slides" into view
+                     offset: {top:100}
+                         });
+                         }
+     })
+}
